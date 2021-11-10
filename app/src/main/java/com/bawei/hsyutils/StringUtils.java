bec56d6fbf4cd0f2b2f0aaac39817807476261bd
我不是保安 <3354047190@qq.com>
2021-11-10 13:11:18 +0800

    '提示'

---
+++ app/src/main/java/com/bawei/hsyutils/StringUtils.java
@@ -0,0 +1,19 @@
+package com.bawei.hsyutils;
+
+public
+/**
+ name:xiaohan
+ time:13:07
+ function:
+ **/
+class StringUtils {
+
+    public static boolean isEmpty(String s){
+        if (s.trim().equals("") || s == null){
+            return true;
+        }
+        return false;
+    }
+
+
+}
